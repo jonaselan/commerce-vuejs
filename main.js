711dce37e658393a6357bcb9a5369e420fdc6367
jonaselan <jonas.elan@gmail.com>
2018-06-18 12:18:39 -0300

    Related a new Vue instance

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-var app = new Vue();
+// Instância do Vue (coração da aplicação)
+var app = new Vue({
+  // relacionando com a div app
+  el: '#app',
+  data: {
+    product: 'Socks',
+    description: 'Put in your feet'
+  }
+});
