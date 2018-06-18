a99ac1630917b1fb4c70e7192ec7746980729f00
jonaselan <jonas.elan@gmail.com>
2018-06-18 12:49:00 -0300

    Add image and verification 'on stock'

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 // Instância do Vue (coração da aplicação)
 var app = new Vue({
-  // relacionando com a div app
+  // relacionando com um elemento id=app
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'Put in your feet'
+    description: 'Put in your feet',
+    image: 'vmSocks-green.jpeg',
+    inventory: '0'
   }
 });
