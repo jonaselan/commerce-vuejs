1e11ee7bb50d5ab11b3abe060fb098bd58b127fb
jonaselan <jonas.elan@gmail.com>
2018-06-18 14:24:56 -0300

    Use computed attributes

--- main.js
+++ main.js
@@ -4,9 +4,8 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Mugs',
-    image: 'https://secure.img1-fg.wfcdn.com/im/31544274/resize-h800%5Ecompr-r85/7431/7431785/Java+Coffee+Mug.jpg',
-    inventory: '20',
-    inStock: false,
+    brand: 'Mbrandgs',
+    selectedVariant: 0,
     details: ["Pretty cool", "For coffee"],
     variants: [
         {
@@ -24,19 +23,33 @@ var app = new Vue({
             backgroundColor: "black",
             border: '1px solid green'
           },
-          quantity: 3,
+          quantity: 0,
           image: "https://shop.pbs.org/ccstore/v1/images/?source=/file/v6723789822436512814/products/CSTM501.gif&height=100&width=100"
         }
       ],
       cart: 0
   },
+  // metódos que podem ser chamados na view
   methods: {
     addToCart: function(){
       // this se refere ao valor dentro de data
       this.cart += 1
     },
-    updateImage(image){
-      this.image = image
+    updateImage(index){
+      this.selectedVariant = index
+    }
+  },
+  // os resultados são armazenados no cache até o momento de sua alteração
+  computed: {
+    title(){
+      return this.brand + ' ' + this.product
+    },
+    image(){
+      return this.variants[this.selectedVariant].image
+    },
+    inStock(){
+      return this.variants[this.selectedVariant].quantity
     }
   }
+
 });
